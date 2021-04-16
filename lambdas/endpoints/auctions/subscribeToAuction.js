4d0d451b6269450e80e790d6a6ea7ceba77f7cff
mauduran <is716122@iteso.mx>
2021-04-15 23:10:44 -0500

    Added function for user to subscribe to auction

---
+++ lambdas/endpoints/auctions/subscribeToAuction.js
@@ -0,0 +1,36 @@
+const Responses = require('../../common/API_Responses');
+const Dynamo = require('../../common/Dynamo');
+const jwt = require("jsonwebtoken");
+
+
+const subscribeUserToAuction = async (email, auctionId) => {
+    let item = {
+        PK: `AUCTION#${auctionId}`,
+        SK: `#INTERESTED_USER#${email}`,
+        interested_user: email,
+        auctionId: auctionId
+    }
+    return await Dynamo.writeIfNotExists(item, 'PK');
+}
+exports.handler = async event => {
+    try {
+        const authorization = event.headers && event.headers.Authorization;
+        const verification = authorization && jwt.verify(authorization, process.env.tokenSecret);
+        
+        if (!verification) return Responses._401({ message: 'Unauthorized' });
+        const email = verification.email;
+        
+        const body = JSON.parse(event.body);
+        if (!body || !body.auctionId) {
+            return Responses._400({ message: 'missing fields in body' });
+        }
+
+        await subscribeUserToAuction(email, body.auctionId);
+        
+        return Responses._200({ success: true, message: "Added subscription" });
+    } catch (error) {
+        console.log(error);
+        return Responses._400({ message: 'Could not subscribe to auction.' });
+    }
+};
+
