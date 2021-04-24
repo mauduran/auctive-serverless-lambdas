b8abbde22ea74fb3c5938fcda5d5aa8dd4cba07c
mauduran <is716122@iteso.mx>
2021-04-24 00:28:34 -0500

    changed webpack config to production

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
     target: 'node',
-    mode: 'none',
+    mode: 'production',
     entry: slsw.lib.entries,
     output: {
         libraryTarget: 'commonjs',
